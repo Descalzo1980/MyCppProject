5fee671d1300f1e12cdcfe453a3ba52ba977e2b0
Leonov Stas <ragioniere1980@gmail.com>
2024-08-17 15:23:52 +0300

    some calculations

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main()
 {
- setlocale(LC_ALL, "Rus");
-
+ setlocale(LC_ALL, "ru");
+/*
  int num1, num2;
  cout << "Введите первое число: ";
  cin >> num1;
@@ -14,6 +14,19 @@ int main()
  cout << "Ответ *: " << num1 * num2 << "\n";
  cout << "Ответ /: " << num1 / num2 << "\n";
  cout << "Ответ -: " << num1 - num2 << "\n";
+
+ int a = 5, b = 44, c = 55, f,g;
+ f = a + b + c;
+ g = (a + b + c) / 3;
+cout << f << endl;
+cout << g << endl;
+*/
+    int a, b, c;
+    cout << "Введите три числа: " << endl;
+    cin >> a >> b >> c;
+    cout << "Сумма трех чисел равна = "<< a + b + c << endl;
+    cout << "Произведение трех чисел равно = "<< a * b *c << endl;
+    cout << "Среднее арифметическое трех чисел равно = "<< (double)(a + b + c) / 3 << endl;
 }
 
 // formate shift + alt + f
