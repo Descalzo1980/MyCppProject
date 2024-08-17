37b40f6ac941aae0b6e59752a5115811ac279418
Leonov Stas <ragioniere1980@gmail.com>
2024-08-17 11:07:47 +0300

    console

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
+#include <locale>
 using namespace std;
 
-int main() 
+int main()
 {
     setlocale(LC_ALL, "Russian");
-    int age = 2;
-    cout << age << endl;
-    age = 10;
-    cout << age << endl;
-    int c, v, b;
-    c = 11;
-    v = 22;
-    b = 33;
-    double d = 2.8;
-    char t = 'K';
-    cout <<c<<" "<<v<<" "<<b<<" "<<t<<endl;
+    int Var, Var2;
 
-    const int DAYS_IN_WEEK = 7;
-    const char NEW_LINE = '\n';
-    const char TAB = '\t';
-    cout << DAYS_IN_WEEK << NEW_LINE;
-    cout << DAYS_IN_WEEK << TAB << TAB << DAYS_IN_WEEK;
-    cout << DAYS_IN_WEEK << NEW_LINE;
+    cout << "Введите первое число: ";
+    cin >> Var;
+    cout << "Введите второе число: ";
+    cin >> Var2;
+    cout << "Число 1 = " << Var << endl
+         << "Число 2 = " << Var2 << endl;
 }
+
+// formate shift + alt + f
