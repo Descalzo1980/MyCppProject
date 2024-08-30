976ce72bfed2b3e1356b93ef5a54c8e1b9a8e2dd
Leonov Stas <ragioniere1980@gmail.com>
2024-08-30 10:07:17 +0300

    pop and push

--- .vscode/crud array.cpp
+++ .vscode/crud array.cpp
@@ -41,6 +41,86 @@ void push_back(int *&arr, int &size, const int value)
     arr = newArray;
 }
 
+void push_front(int *&arr, int &size, const int value)
+{
+    int *newArray = new int[size + 1];
+    newArray[0] = value;
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i + 1] = arr[i];
+    }
+
+    size++;
+    delete[] arr;
+    arr = newArray;
+}
+
+void push_diff(int *&arr, int &size, const int value, const int index)
+{
+    if (index < 0 || index > size)
+    {
+        return;
+    }
+
+    int *newArray = new int[size + 1];
+    for (int i = 0; i < index; i++)
+    {
+        newArray[i] = arr[i];
+    }
+    newArray[index] = value;
+    for (int i = index; i < size; i++)
+    {
+        newArray[i + 1] = arr[i];
+    }
+    size++;
+    delete[] arr;
+    arr = newArray;
+}
+
+void pop_diff(int *&arr, int &size, const int index)
+{
+    // Проверяем, что индекс корректен
+    if (index < 0 || index >= size)
+    {
+        return;
+    }
+
+    // Уменьшаем размер массива
+    size--;
+    int *newArray = new int[size];
+
+    // Копируем элементы до удаляемого
+    for (int i = 0; i < index; i++)
+    {
+        newArray[i] = arr[i];
+    }
+
+    // Копируем элементы после удаляемого
+    for (int i = index; i < size; i++)
+    {
+        newArray[i] = arr[i + 1];
+    }
+
+    // Освобождаем память старого массива
+    delete[] arr;
+
+    // Перенаправляем указатель
+    arr = newArray;
+}
+
+void pop_front(int *&arr, int &size)
+{
+    size--;
+    int *newArray = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = arr[i + 1];
+    }
+    delete[] arr;
+    arr = newArray;
+}
+
 void pop_back(int *&arr, int &size)
 {
     size--;
@@ -67,6 +147,17 @@ int main()
     pop_back(arr, size);
     ShowArray(arr, size);
     pop_back(arr, size);
+    ShowArray(arr, size);
+
+    push_front(arr, size, 111);
+    ShowArray(arr, size);
+
+    pop_front(arr, size);
+    ShowArray(arr, size);
+
+    push_diff(arr, size, 111, 2);
+    ShowArray(arr, size);
+    pop_diff(arr, size, 2);
     ShowArray(arr, size);
     delete[] arr;
     system("pause");
