9cc06d85bfc965ff9be104a417aecdc1662c7bfe
Leonov Stas <ragioniere1980@gmail.com>
2024-09-07 00:10:59 +0300

    friend

---
+++ .vscode/oop13 friendly fun.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+class Test;
+class Point
+{
+private:
+  int x;
+  int y;
+
+public:
+  Point(int valueX, int valueY)
+  {
+    x = valueX;
+    y = valueY;
+  }
+
+  Point()
+  {
+    x = 0;
+    y = 0;
+  }
+
+  Point operator+(const Point &other)
+  {
+    Point temp;
+    temp.x = this->x + other.x;
+    temp.y = this->y + other.y;
+    return temp;
+  }
+
+  int getX() const { return x; }
+  int getY() const { return y; }
+
+  Point &operator++()
+  {
+    this->x++;
+    this->y += 1;
+    return *this;
+  }
+
+  Point operator++(int)
+  {
+    Point temp(*this);
+    this->x++;
+    this->y++;
+    return temp;
+  }
+
+  Point &operator--()
+  {
+    this->x--;
+    this->y -= 1;
+    return *this;
+  }
+
+  Point operator--(int)
+  {
+    Point temp(*this);
+    this->x--;
+    this->y--;
+    return temp;
+  }
+
+  void Print()
+  {
+    cout << "X = " << x << "\t Y = " << y << endl
+         << endl;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Point &point)
+  {
+    os << "Point(" << point.x << ", " << point.y << ")";
+    return os;
+  }
+
+  friend void ChangeX(Point &value, Test &valueTest);
+};
+
+
+class TestClass
+{
+public:
+  int &operator[](int index)
+  {
+    return arr[index];
+  }
+
+private:
+  int arr[5]{5, 44, 777, 789, 99};
+};
+
+class Test
+{
+  private:
+  int Data = 0;
+
+    friend void ChangeX(Point &value, Test &valueTest);
+};
+
+void ChangeX(Point &value, Test &valueTest)
+{
+  value.x = -1;
+  valueTest.Data = -5555;
+}
+
+int main()
+{
+  SetConsoleCP(65001);
+  SetConsoleOutputCP(65001);
+  setlocale(LC_ALL, "ru_RU.UTF-8");
+
+  TestClass a;
+  cout << a[0] << endl;
+  a[0] = 100;
+  cout << a[0] << endl;
+
+  Point point(5, 12);
+  point.Print();
+
+  Test test;
+  ChangeX(point,test);
+  point.Print();
+  system("pause");
+  return 0;
+}
