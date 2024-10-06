578b096a1c4050c21f9f88bdf0c3cbcbffe3ab36
Leonov Stas <ragioniere1980@gmail.com>
2024-10-07 00:08:12 +0300

    yet vector

--- .vscode/stl 1.cpp
+++ .vscode/stl 1.cpp
@@ -21,7 +21,8 @@ int main()
     SetConsoleOutputCP(65001);
     setlocale(LC_ALL, "ru_RU.UTF-8");
 
-    vector<int> myVector;
+    vector<int> myVector = {1, 2, 3, 4, 5};
+    myVector.reserve(100);
     myVector.push_back(2);
     myVector.push_back(3);
     myVector.push_back(4);
@@ -30,13 +31,50 @@ int main()
 
     myVector[1] = 1000;
 
-    cout << myVector[10] << endl;
+    cout << myVector[2] << endl;
+    // cout << myVector.at(22) << endl; // проверяет границы
+
+    /*     try
+        {
+            cout << myVector.at(22) << endl;
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        } */
+
     cout << "количество элементов в векторе " << myVector.size() << endl;
+
+    /*     myVector.clear();
+
+        cout << "количество элементов в векторе " << myVector.size() << endl; */
 
     for (int i = 0; i < myVector.size(); i++)
     {
         cout << myVector[i] << endl;
     }
+    /*
+        myVector.pop_back(); */
+
+    cout << "количество элементов в векторе " << myVector.size() << endl;
+
+    cout << "capacity в векторе " << myVector.capacity() << endl;
+
+    myVector.shrink_to_fit();
+
+    cout << "capacity в векторе после shrink_to_fit " << myVector.capacity() << endl;
+
+    cout << "empty? " << myVector.empty() << endl;
+
+    myVector.resize(20);
+
+    for (int i = 0; i < myVector.size(); i++)
+    {
+        cout << myVector[i] << endl;
+    }
+
+    myVector.insert();
+    myVector.erase();
 
     system("pause");
     return 0;
