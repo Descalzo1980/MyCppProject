3151869331cb6504426690e273b4af6486171f88
Leonov Stas <ragioniere1980@gmail.com>
2024-10-14 21:01:43 +0300

    array

---
+++ .vscode/stl 5 array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <memory>
+#include <typeinfo>
+#include <array>
+using namespace std;
+
+/*
+
+STL
+
+array
+
+*/
+
+int main()
+{
+
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    
+
+    system("pause");
+    return 0;
+}
