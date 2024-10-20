6ed41397f053f07214d42648842ffae6b81dedee
Leonov Stas <ragioniere1980@gmail.com>
2024-10-20 12:29:40 +0300

    ololo

--- .vscode/dynamic_data_structures 3 binary tree.cpp
+++ .vscode/dynamic_data_structures 3 binary tree.cpp
@@ -27,8 +27,57 @@ using namespace std;
  /  \     \
 [3] [7]   [20] ← Листья (узлы без детей)
 
+логика добавления 
+
+
+      [50]
+     /    \
+   [45]    [55]  если больше то вправо, если меньше то влево, и только два потомка
+   /  \
+ [1] [49] большее вправо!
+
+с с++ это в мапе используется, или реализация ассоциативных массивов
 
 */
+
+class MyBT {
+    struct Node {
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int val) : value(val), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+
+public:
+    MyBT() : root(nullptr) {}
+
+    void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    friend ostream& operator<<(ostream& os, const MyBT& tree) {
+        tree.printInOrder(os, tree.root);
+        return os;
+    }
+
+private:
+    Node* addRecursive(Node* current, int value) {
+        if (current == nullptr) {
+            return new Node(value);  // Создаем новый узел, если текущий пуст
+        }
+
+        if (value < current->value) {
+            current->left = addRecursive(current->left, value);  // Добавляем слева
+        } else if (value > current->value) {
+            current->right = addRecursive(current->right, value);  // Добавляем справа
+        }
+
+        return current;  // Возвращаем текущий узел
+    }
+};
 
 int main()
 {
@@ -37,7 +86,14 @@ int main()
     SetConsoleOutputCP(65001);
     setlocale(LC_ALL, "ru_RU.UTF-8");
 
-    
+    MyBT tree;
+    tree.add(10);
+    tree.add(5);
+    tree.add(15);
+    tree.add(3);
+    tree.add(7);
+
+    cout << tree << endl;
     
     system("pause");
     return 0;
---
+++ build/CMakeFiles/MyCppProject.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/MyCppProject/.vscode/about_switch.cpp" "CMakeFiles/MyCppProject.dir/.vscode/about_switch.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/about_switch.cpp.obj.d"
+  "D:/MyCppProject/.vscode/arrays.cpp" "CMakeFiles/MyCppProject.dir/.vscode/arrays.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/arrays.cpp.obj.d"
+  "D:/MyCppProject/.vscode/calculator.cpp" "CMakeFiles/MyCppProject.dir/.vscode/calculator.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/calculator.cpp.obj.d"
+  "D:/MyCppProject/.vscode/cont.cpp" "CMakeFiles/MyCppProject.dir/.vscode/cont.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/cont.cpp.obj.d"
+  "D:/MyCppProject/.vscode/debagger.cpp" "CMakeFiles/MyCppProject.dir/.vscode/debagger.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/debagger.cpp.obj.d"
+  "D:/MyCppProject/.vscode/dowhile.cpp" "CMakeFiles/MyCppProject.dir/.vscode/dowhile.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/dowhile.cpp.obj.d"
+  "D:/MyCppProject/.vscode/enter_sign.cpp" "CMakeFiles/MyCppProject.dir/.vscode/enter_sign.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/enter_sign.cpp.obj.d"
+  "D:/MyCppProject/.vscode/for.cpp" "CMakeFiles/MyCppProject.dir/.vscode/for.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/for.cpp.obj.d"
+  "D:/MyCppProject/.vscode/goto.cpp" "CMakeFiles/MyCppProject.dir/.vscode/goto.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/goto.cpp.obj.d"
+  "D:/MyCppProject/.vscode/nested_loops.cpp" "CMakeFiles/MyCppProject.dir/.vscode/nested_loops.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/nested_loops.cpp.obj.d"
+  "D:/MyCppProject/.vscode/nested_loops copy.cpp" "CMakeFiles/MyCppProject.dir/.vscode/nested_loops_copy.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/nested_loops_copy.cpp.obj.d"
+  "D:/MyCppProject/.vscode/parity.cpp" "CMakeFiles/MyCppProject.dir/.vscode/parity.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/parity.cpp.obj.d"
+  "D:/MyCppProject/.vscode/while.cpp" "CMakeFiles/MyCppProject.dir/.vscode/while.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/.vscode/while.cpp.obj.d"
+  "D:/MyCppProject/boolean.cpp" "CMakeFiles/MyCppProject.dir/boolean.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/boolean.cpp.obj.d"
+  "D:/MyCppProject/debagger.cpp" "CMakeFiles/MyCppProject.dir/debagger.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/debagger.cpp.obj.d"
+  "D:/MyCppProject/ifelse.cpp" "CMakeFiles/MyCppProject.dir/ifelse.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/ifelse.cpp.obj.d"
+  "D:/MyCppProject/main.cpp" "CMakeFiles/MyCppProject.dir/main.cpp.obj" "gcc" "CMakeFiles/MyCppProject.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
