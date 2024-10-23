b6606df5579e142181ed743e9d43fdaa5b49f6b3
Leonov Stas <ragioniere1980@gmail.com>
2024-10-23 19:38:43 +0300

    set multiset

--- .vscode/dynamic_data_structures 8 set mset.cpp
+++ .vscode/dynamic_data_structures 8 set mset.cpp
@@ -25,7 +25,11 @@ int main()
     SetConsoleOutputCP(65001);
     setlocale(LC_ALL, "ru_RU.UTF-8");
 
-    set<int> mySet = {5,4,7,8,22};
+/*     set<int> mySet = {5, 4, 7, 8, 22};
+
+    int value;
+    cout << "Какое число ищем?" << endl;
+    cin >> value; */
 
     /*     mySet.insert(5);
         mySet.insert(1);
@@ -39,17 +43,45 @@ int main()
             cout << item << endl;
         } */
 
-/*     for (int i = 0; i < 20; ++i)
+    /*     for (int i = 0; i < 20; ++i)
+        {
+            mySet.insert(rand() % 10);
+        }
+
+        for (auto &item : mySet)
+        {
+            cout << item << endl;
+        } */
+
+    /*     auto it = mySet.find(5); */
+
+/*     if (mySet.find(value) != mySet.end())
     {
-        mySet.insert(rand() % 10);
+        cout << "число " << value << " найдено " << endl;
     }
+    else
+    {
+        cout << "число " << value << " отсутствует" << endl;
+    }
+
+    for (auto &item : mySet)
+    {
+        cout << item << endl;
+    }
+
+    mySet.erase(5555);
 
     for (auto &item : mySet)
     {
         cout << item << endl;
     } */
 
-    auto it = mySet.find(5);
+    multiset<int> myMs = {1,2,3,4,4,5,5,56,66,6,6,66}; // можно хранить одинаковые зачения
+
+        for (auto &item : myMs)
+    {
+        cout << item << endl;
+    }
 
     system("pause");
     return 0;
