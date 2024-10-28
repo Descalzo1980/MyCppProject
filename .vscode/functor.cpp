4ae3ac785e7f7f00014f8124773100791efebce0
Leonov Stas <ragioniere1980@gmail.com>
2024-10-28 09:17:57 +0300

    functor

---
+++ .vscode/functor.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <thread>
+
+using namespace std;
+
+/*
+    Функторы
+    (); это функтор. объект класса может вести себя как функция
+ */
+
+class functor
+{
+private:
+public:
+    int operator()(int a, int b)
+    {
+        cout << "я функтор " << endl;
+        return a + b;
+    }
+};
+
+class EvenFunctor
+{
+private:
+    int evenSum = 0;
+    int evenCount = 0;
+
+public:
+    void operator()(int value)
+    {
+        if (value % 2 == 0)
+        {
+            evenSum += value;
+            evenCount++;
+        }
+    }
+
+    void ShowEvenSum()
+    {
+        cout << "сумма чётных чисел = " << evenSum << endl;
+    }
+
+    void ShowEvenCount()
+    {
+        cout << "всего чётных чисел = " << evenCount << endl;
+    }
+};
+
+int main()
+{
+
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    functor f;
+    int result = f(5, 2);
+
+    cout << result << endl;
+
+
+    EvenFunctor ef;
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    for (auto el : arr)
+    {
+        ef(el);
+    }
+
+    ef.ShowEvenCount();
+    ef.ShowEvenSum();
+
+    system("pause");
+
+    return 0;
+}
