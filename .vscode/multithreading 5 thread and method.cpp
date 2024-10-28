fd3e82af0ebcab598843f13bb8af4fac34bcc761
Leonov Stas <ragioniere1980@gmail.com>
2024-10-28 10:11:13 +0300

    running a class method in a separate thread

---
+++ .vscode/multithreading 5 thread and method.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <thread>
+#include <chrono>
+#include <vector>
+using namespace std;
+
+/*
+
+Многопоточность
+Запуск метода класса в отдельном потоке
+
+*/
+
+class MyClass
+{
+public:
+    void DoWork()
+    {
+        this_thread::sleep_for(chrono::milliseconds(2000));
+
+        cout << "ID потока = " << this_thread::get_id() << " DoWork Start" << endl;
+
+        this_thread::sleep_for(chrono::milliseconds(5000));
+
+        cout << "ID потока = " << this_thread::get_id() << " DoWork Stopped" << endl;
+    }
+
+    void DoWork2(int a)
+    {
+        this_thread::sleep_for(chrono::milliseconds(2000));
+
+        cout << "ID потока = " << this_thread::get_id() << " DoWork Start" << endl;
+
+        this_thread::sleep_for(chrono::milliseconds(5000));
+
+        cout << "DoWork2 значение параметра\t" << a << endl;
+
+        cout << "ID потока = " << this_thread::get_id() << " DoWork Stopped" << endl;
+    }
+
+    int Sum(int a, int b)
+    {
+        this_thread::sleep_for(chrono::milliseconds(2000));
+
+        cout << "ID потока = " << this_thread::get_id() << " Sum Start" << endl;
+
+        this_thread::sleep_for(chrono::milliseconds(5000));
+
+        cout << "ID потока = " << this_thread::get_id() << " Sum Stopped" << endl;
+
+        return a + b;
+    }
+};
+
+int main()
+{
+
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    int result;
+    MyClass m;
+
+    thread t([&]()
+             { result = m.Sum(2, 5); });
+
+    for (size_t i = 0; i <= 10; i++)
+    {
+        cout << "ID потока = " << this_thread::get_id() << "\tmain works\t" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+
+    t.join();
+
+    cout << "Result\t " << result << endl;
+
+    system("pause");
+    return 0;
+}
